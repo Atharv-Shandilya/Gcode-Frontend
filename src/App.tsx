43ce1a8cacd3dd12547c8137e506a9b8d99e28d9
Atharva Shandilya <atharvashandilya887@gmail.com>
2025-12-05 14:06:29 +0530

    Add Basic Navigation UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import ProfileCard from "./components/ProfileCard";
 import ProfileView from "./components/ProfileView";
 import BookingOverlay from "./components/BookingOverlay";
 import GlobalStore from "./store/GlobalStore";
+import Nav from "./components/Nav";
 
 function App() {
   const showBooking = GlobalStore((state) => state.showBooking);
   return (
-    <main className="p-2 relative">
-      <ProfileCard />
-      <ProfileView />
-      {showBooking && <BookingOverlay />}
+    <main className=" relative">
+      <Nav />
+      <article className="p-2">
+        <ProfileCard />
+        <ProfileView />
+        {showBooking && <BookingOverlay />}
+      </article>
     </main>
   );
 }
